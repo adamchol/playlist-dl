facf2a267d94aeed6329b90c1823dfd9063a1881
Richard Shank <itsrichardshank@gmail.com>
2024-07-28 21:55:00 +0200

    fix: change the module link import according to the GitHub repo

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2024 Adam Cholewiński ITSRICHARDSHANK@GMAIL.COM
 */
 package main
 
-import "github.com/richardshank/playlist-dl/cmd"
+import "github.com/adamchol/playlist-dl/cmd"
 
 func main() {
 	cmd.Execute()
